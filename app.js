94a063d05b3657be1d4cd29c97a1dff9c60bc47a
Xiaowei Huang <x68huang.uwaterloo.ca>
2017-02-07 16:32:36 -0600

    Add jslint support and jsconfig.json

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*jslint node: true */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -19,7 +20,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,14 +32,14 @@ app.get('/hello', hello);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
